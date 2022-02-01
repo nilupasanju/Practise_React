71062adde0a06c8570be98743e15a434c73ed612
Nilupa Dhanawalavithanage <nilu@Nilupas-MacBook-Pro.local>
2022-02-01 15:35:04 +1100

    Intial comment Practise React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const lakeList = [
+  "Echo Lake",
+  "Beeta Lake",
+  "Alpha Lake"
+];
+
+
+
+//function Lake(props) {
+  //return <h1> {props.name}</h1>
+//}
+
+//function Hello({library, message, number}) {
+  //return (
+   // <div>
+      //<hi>Welcome to {library}</hi>
+      //<p>{message}</p>
+      //<p>{number} Props Total</p>
+   // </div>
+  //);
+//}
+
+
+function App(props) {
+  return (
+     //create element (element, where we need to render) arguments(name of tag, propeties, children)
+    //React.createElement("h1", null, "Hello!" ),
+
+    //<div >
+    // <Lake name="Lake Tahoe" />
+    // <Lake name="Nayagara Lake" />
+    //</div>
+
+    <div> Eventual Lakes</div>,
+    <ul>
+      {props.lakes.map(lake => (
+        <li>{lake}</li>
+      )
+        )}
+    </ul>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<Hello
+    //library="React"
+   // message="add dynamic data"
+   // number={3}
+  ///>,
+  <App lakes={lakeList}/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//
