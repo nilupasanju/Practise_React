66f9b5ac6921d32af0212f9987742e39be600d4c
Nilupa Dhanawalavithanage <nilu@Nilupas-MacBook-Pro.local>
2022-02-12 22:11:43 +1100

    Updated

--- src/index.js
+++ src/index.js
@@ -2,39 +2,96 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
+let city = {
+  name ="Madrid",
+  country= "Spain"
+};
+
+ReactDOM.render(
+ <h1>
+  {city.name} is in {city.country}
+ </h1>,
+
+ document.getElementById('root')
+);
+
+*/
+
+/*
+function Hello() {
+  return (
+    <div>
+      <hi>Welcome to React</hi>
+      <p>Lets build something</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Hello  />,
+  document.getElementById('root')
+);
+*/
+
+
+
+/*
+function Hello({library, message, number}) {
+  return (
+    <div>
+      <hi>Welcome to {library}</hi>
+      <p>{message}</p>
+      <p>{number} Props Total</p>
+    </div>
+  );
+}
+
+ReactDom.render(
+ <Hello
+    library="React"
+   message="add dynamic data"
+   number={3}
+  />,
+
+ document.getElementById('root')
+);
+*/
+
+//*** Composing Component
+
+/*
+function Lake(props) {
+  return <h1> {props.name}</h1>
+}
+
+Function App(props){
+  return (
+    <div >
+      <Lake name="Lake Tahoe" />,
+      <Lake name="Nayagara Lake" />
+    </div>
+
+  );
+}
+
+ReactDom.render(
+ <App/ >,
+ document.getElementById('root')
+);
+*/
+
+//***** Rendering List
+
+/*
 const lakeList = [
   "Echo Lake",
   "Beeta Lake",
   "Alpha Lake"
 ];
 
-
-
-//function Lake(props) {
-  //return <h1> {props.name}</h1>
-//}
-
-//function Hello({library, message, number}) {
-  //return (
-   // <div>
-      //<hi>Welcome to {library}</hi>
-      //<p>{message}</p>
-      //<p>{number} Props Total</p>
-   // </div>
-  //);
-//}
-
-
 function App(props) {
   return (
-     //create element (element, where we need to render) arguments(name of tag, propeties, children)
-    //React.createElement("h1", null, "Hello!" ),
-
-    //<div >
-    // <Lake name="Lake Tahoe" />
-    // <Lake name="Nayagara Lake" />
-    //</div>
-
     <div> Eventual Lakes</div>,
     <ul>
       {props.lakes.map(lake => (
@@ -46,12 +103,35 @@ function App(props) {
 }
 
 ReactDOM.render(
-  //<Hello
-    //library="React"
-   // message="add dynamic data"
-   // number={3}
-  ///>,
+
   <App lakes={lakeList}/>,
   document.getElementById('root')
+);*/
+
+// ****** Rendering List Object
+/*
+const lakeList = [
+  {id: "1", name: "Echo", trailhead: "Echo"},
+  {id: "2", name: "Maud", trailhead: "Wrights"},
+  {id: "3", name: "Velma", trailhead: "Bayview"}
+];
+
+function App({ lakes}) {
+  return(
+    <div>
+      {lakes.map(lake => (
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accesed by: {lake.trailhead}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App lakes={lakeList}/>,
+  document.getElementById('root')
+
 );
-//
+*/
